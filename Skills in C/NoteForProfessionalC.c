64a6d0487e2e575e2730bf05535e2e3f514fe623
Kleiton Da Rosa <82580588+KleitonDaRosa@users.noreply.github.com>
2021-05-22 16:38:59 +0100

    Add files via upload

---
+++ Skills in C/NoteForProfessionalC.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define MAX 100
+
+
+
+int main (){
+    
+int vetor[MAX];
+    
+int valoresPretendidos,numeroProcurado;
+ 
+   
+    
+printf("Inserir quantos Valores pretendes Inserir>>");
+    
+scanf("%d",&valoresPretendidos);
+ 
+   
+    
+for (int i=0 ; i < valoresPretendidos ; i++ ){
+        
+	printf("Inserir o valor %d>>",i+1);
+        
+	scanf("%d",&vetor[i]);
+    }
+    
+    
+
+//Imprimir valores do vector<> ;
+
+for (int j =0 ; j < valoresPretendidos ; j++){
+
+        printf("%d\t\7",vetor[j]);
+    }
+ 
+   
+}
